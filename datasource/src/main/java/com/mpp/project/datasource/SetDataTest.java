15b81d4354e0386bbeec8cdaf03983fe3f682bd8
LawrenceHong <2395935332@qq.com>
2017-03-08 10:14:14 -0600

    Set default ip

--- datasource/src/main/java/com/mpp/project/datasource/SetDataTest.java
+++ datasource/src/main/java/com/mpp/project/datasource/SetDataTest.java
@@ -7,10 +7,10 @@ public class SetDataTest {
     public static void main(String[] args) {
         RedisHelper reditHelper = new RedisHelper("localhost", 6379);
 
-        String administrator1 = "{\"staffID\":\"administrator1\",\"passwd\":\"123456\",\"permits\":[\"PERMIT_ALL\"]}";
-        String administrator2 = "{\"staffID\":\"administrator2\",\"passwd\":\"123456\",\"permits\":[\"PERMIT_ADD_MEMBER\",\"PERMIT_EDIT_MEMBER\", \"PERMIT_ADD_BOOK\", \"PERMIT_ADD_COPY\"]}";
-        String librarian1 = "{\"staffID\":\"librarian1\",\"passwd\":\"123456\",\"permits\":[\"PERMIT_CHECKOUT\"]}";
-        String librarian2 = "{\"staffID\":\"librarian2\",\"passwd\":\"123456\",\"permits\":[\"PERMIT_CHECKOUT\",\"PERMIT_ADD_BOOK\", \"PERMIT_ADD_MEMBER\", \"PERMIT_EDIT_MEMBER\"]}";
+        String administrator1 = "{\"staffID\":\"administrator1\",\"passwd\":\"123456\",\"permits\":[\"PERMIT_BOTH\"]}";
+        String administrator2 = "{\"staffID\":\"administrator2\",\"passwd\":\"123456\",\"permits\":[\"PERMIT_ADMIN\"]}";
+        String librarian1 = "{\"staffID\":\"librarian1\",\"passwd\":\"123456\",\"permits\":[\"PERMIT_LIBRARIAN\"]}";
+        String librarian2 = "{\"staffID\":\"librarian2\",\"passwd\":\"123456\",\"permits\":[\"PERMIT_LIBRARIAN\"]}";
 
         reditHelper.SetInfo("administrator1", administrator1);
         reditHelper.SetInfo("administrator2", administrator2);
